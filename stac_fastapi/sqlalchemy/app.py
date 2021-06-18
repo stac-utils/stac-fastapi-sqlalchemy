c3bfe94aaaaf095ef51a9e97991fdd181f8d8b05
David Bitner <bitner@dbspatial.com>
2021-06-17 20:22:48 -0500

    PGStac  (#126)
    * add .envrc to .*ignore
    
    * submodules all as one package
    
    * submodules all as one package
    
    * use namespace packages
    
    * remove setup.py from pgrest
    
    * update cicd workflow
    
    * cherry pick pgstac branch to updated package layout
    
    * allow async endpoints
    
    * core modifications needed for pgstac
    
    * cleanup
    
    * cleanup
    
    * setup console scripts
    
    * get transaction api working
    
    * bring in pgstac
    
    * remove old pgrest
    
    * bring Dockerfile over
    
    * cherrypicks
    
    * add removed readme back
    
    * linting, add gzip middleware
    
    * move mangum to awslambda extras
    
    * sorts
    
    * fixes for precommit
    
    * fixes for precommit
    
    * uggh pydocstyle
    
    * pgstac tests and fixes from tests
    
    * get test runners working
    
    * linting
    
    * tag for db compatible with pgstac
    
    * db docker uses different ENV vars
    
    * linted too hard
    
    * test cleanup
    
    * docker env fixes, documentation
    
    * docker env fixes, documentation
    
    * switch from gzip to brotli
    
    * Update stac_fastapi/api/stac_fastapi/api/app.py
    
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>
    
    * Update stac_fastapi/pgstac/setup.py
    
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>
    
    * Update stac_fastapi/pgstac/README.md
    
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>
    
    * fix issues in links for base_url and items relations type
    
    * fix docstring quotes
    
    * add item_id, collection_id in response to #138
    
    * update pypgstac version
    
    * fix parent link
    
    * change id key to items_id in delete_item
    
    * update version of pgstac
    
    * Update docker-compose.yml
    
    call pgstac app as python module rather than using the cli command
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>
    
    * Update stac_fastapi/pgstac/README.md
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>
    
    * Update stac_fastapi/pgstac/stac_fastapi/pgstac/models/links.py
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>
    
    * Update stac_fastapi/pgstac/tests/clients/test_postgres.py
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>
    
    * Update stac_fastapi/pgstac/tests/clients/test_postgres.py
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>
    
    * Apply suggestions from code review
    
    remove unused load_test_data fixtures
    
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>
    
    * fixes from code review. remove unused/debug logging. remove unused functions. add type hints.
    
    * add import for asyncpg.pool for type hint
    
    * add default for initial pool and connection value
    
    * use request.json() because we are adding in the paging token to the POST request
    
    * linting fixes
    
    * make docker-compose use reload=false for uvicorn to avoid any issues with venvs
    
    * increase number of connections on pg instance, only mount scripts and stac_fastapi directories
    
    * move test data and add test data to both pgstac and sqlalchemy in docker, make no collections return empty array
    
    * fix test for collections returning empty array rather than not found
    
    * add exclude_unset=True when dumping data to json to add in transactions extension
    
    Co-authored-by: Kyle Barron <kylebarron2@gmail.com>
    Co-authored-by: Jeff Albrecht <jeff@arturo.ai>

--- stac_fastapi/sqlalchemy/app.py
+++ stac_fastapi/sqlalchemy/app.py
@@ -14,6 +14,7 @@ from stac_fastapi.sqlalchemy.transactions import (
     BulkTransactionsClient,
     TransactionsClient,
 )
+from stac_fastapi.sqlalchemy.types.search import SQLAlchemySTACSearch
 
 settings = SqlalchemySettings()
 session = Session.create_from_settings(settings)
@@ -27,5 +28,39 @@ api = StacApi(
         SortExtension(),
     ],
     client=CoreCrudClient(session=session),
+    search_request_model=SQLAlchemySTACSearch,
 )
 app = api.app
+
+
+def run():
+    """Run app from command line using uvicorn if available."""
+    try:
+        import uvicorn
+
+        uvicorn.run(
+            "stac_fastapi.sqlalchemy.app:app",
+            host=settings.app_host,
+            port=settings.app_port,
+            log_level="info",
+            reload=settings.reload,
+        )
+    except ImportError:
+        raise RuntimeError("Uvicorn must be installed in order to use command")
+
+
+if __name__ == "__main__":
+    run()
+
+
+def create_handler(app):
+    """Create a handler to use with AWS Lambda if mangum available."""
+    try:
+        from mangum import Mangum
+
+        return Mangum(app)
+    except ImportError:
+        return None
+
+
+handler = create_handler(app)
